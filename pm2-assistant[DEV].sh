53d040f6298a3e81fc91276fb237b71071009486
ALEX SIRAIT <alexsirait1001@gmail.com>
2024-12-19 15:51:16 +0700

    PUSH

--- pm2-assistant[DEV].sh
+++ pm2-assistant[DEV].sh
@@ -5,8 +5,8 @@ HOST="192.168.88.60"
 APP_NAME="assistant"
 
 # Definisikan variabel
-DJANGO_DIR="/var/lib/jenkins/workspace/$APP_NAME/mysatnusa"
-VENV_DIR="/var/lib/jenkins/workspace/$APP_NAME/venv"
+DJANGO_DIR="/var/www/$APP_NAME/mysatnusa"
+VENV_DIR="/var/www/$APP_NAME/venv"
 WORKERS=9  # Jumlah workers untuk 4 CPU Core
 THREADS=2  # Jumlah thread per worker
 MAX_REQUESTS=1000  # Batas permintaan per worker
